02beee1ab36f0fcd3080421b671876c28d79082d
bernasantiago <bernayilmaz0735@gmail.com>
2024-05-07 13:36:56 -0700

    login page updated and adjusted properties file based on feature file.

--- src/test/java/com/group5_sprint2_cloud/pages/LoginPage.java
+++ src/test/java/com/group5_sprint2_cloud/pages/LoginPage.java
@@ -5,22 +5,21 @@ import com.group5_sprint2_cloud.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 public class LoginPage {
 
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(id="prependedInput")
+    @FindBy(xpath = "//input[@id='user']")
     public WebElement userName;
 
 
 
-    @FindBy(id="prependedInput2")
+    @FindBy(xpath = "//*[@id='password']")
     public WebElement password;
 
-    @FindBy(name = "_submit")
+    @FindBy(xpath = "//input[@id='submit-form']")
     public WebElement submit;
 
 
