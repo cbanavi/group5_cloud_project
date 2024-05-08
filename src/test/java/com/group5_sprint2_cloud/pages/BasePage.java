8d3568262e7a967bc50174de0b5265b452ff8629
Chiyavan Banavi <cbanavi@gmail.com>
2024-05-07 21:37:23 -0700

    Created a navigateToModule method in BasePage

--- src/test/java/com/group5_sprint2_cloud/pages/BasePage.java
+++ src/test/java/com/group5_sprint2_cloud/pages/BasePage.java
@@ -67,32 +67,21 @@ public abstract class BasePage {
     }
 
 
-
     /**
-     * This method will navigate user to the specific module in vytrack application.
-     * For example: if tab is equals to Activities, and module equals to Calls,
-     * Then method will navigate user to this page: http://qa2.vytrack.com/call/
-     *
-     * @param tab
+     * This method will click any module you put. Just extend into your StepDef class
+     * In your gerkin step, call navigateToModule() and put whichever module you'd like to click
+     * Ex. navigateToModule("Photos")
      * @param module
      */
-    public void navigateToModule(String tab, String module) {
-        String tabLocator = "//span[normalize-space()='" + tab + "' and contains(@class, 'title title-level-1')]";
-        String moduleLocator = "//span[normalize-space()='" + module + "' and contains(@class, 'title title-level-2')]";
-        try {
-            BrowserUtils.waitForClickablility(By.xpath(tabLocator), 5);
-            WebElement tabElement = Driver.getDriver().findElement(By.xpath(tabLocator));
-            new Actions(Driver.getDriver()).moveToElement(tabElement).pause(200).doubleClick(tabElement).build().perform();
-        } catch (Exception e) {
-            BrowserUtils.clickWithWait(By.xpath(tabLocator), 5);
-        }
+    public void navigateToModule(String module) {
+        String moduleLocator = "(//a[@aria-label='"+module+"'])[1]";
+
         try {
             BrowserUtils.waitForPresenceOfElement(By.xpath(moduleLocator), 5);
             BrowserUtils.waitForVisibility(By.xpath(moduleLocator), 5);
             BrowserUtils.scrollToElement(Driver.getDriver().findElement(By.xpath(moduleLocator)));
             Driver.getDriver().findElement(By.xpath(moduleLocator)).click();
         } catch (Exception e) {
-//            BrowserUtils.waitForStaleElement(Driver.get().findElement(By.xpath(moduleLocator)));
             BrowserUtils.clickWithTimeOut(Driver.getDriver().findElement(By.xpath(moduleLocator)),  5);
         }
     }
