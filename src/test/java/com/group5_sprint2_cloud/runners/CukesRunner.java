1385a77c87aee8ecb79ef5db594e004cafcf6240
Chiyavan Banavi <cbanavi@gmail.com>
2024-05-08 23:22:18 -0700

    Updating .gitignore

--- src/test/java/com/group5_sprint2_cloud/runners/CukesRunner.java
+++ src/test/java/com/group5_sprint2_cloud/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/group5_sprint2_cloud/step_definitions",
         dryRun = false,
-        tags = "@wip1233",
+        tags = "@wip",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
