58ddf1507b29d28f6ffaaf9dd5245ea36ad78740
Chiyavan Banavi <cbanavi@gmail.com>
2024-05-08 23:20:50 -0700

    Updating .gitignore

---
+++ src/test/java/com/group5_sprint2_cloud/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.group5_sprint2_cloud.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+        },
+        features = "src/test/resources/features",
+        glue = "com/group5_sprint2_cloud/step_definitions",
+        dryRun = false,
+        tags = "@wip1233",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
+
+
