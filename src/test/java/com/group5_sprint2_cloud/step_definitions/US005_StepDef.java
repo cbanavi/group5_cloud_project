5a0756cea231dbdc71a087d5c0a7df02383deea5
bernasantiago <bernayilmaz0735@gmail.com>
2024-05-14 13:32:29 -0700

    Calendar feature is done.

--- src/test/java/com/group5_sprint2_cloud/step_definitions/US005_StepDef.java
+++ src/test/java/com/group5_sprint2_cloud/step_definitions/US005_StepDef.java
@@ -2,6 +2,8 @@ package com.group5_sprint2_cloud.step_definitions;
 
 import com.group5_sprint2_cloud.pages.BasePage;
 import com.group5_sprint2_cloud.pages.CalendarPage_Berna;
+import com.group5_sprint2_cloud.pages.LoginPage;
+import com.group5_sprint2_cloud.utilities.BrowserUtils;
 import com.group5_sprint2_cloud.utilities.ConfigurationReader;
 import com.group5_sprint2_cloud.utilities.Driver;
 import io.cucumber.java.en.And;
@@ -9,22 +11,20 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.java.eo.Se;
+import org.checkerframework.checker.units.qual.A;
 import org.checkerframework.checker.units.qual.C;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.lang.module.Configuration;
 
 public class US005_StepDef extends BasePage {
 
-   // @Given("user is on the dashboard page")
-   // public void user_is_on_the_dashboard_page() {
-
-
-  //  }
-
+    String eventName;
+    Actions actions = new Actions(Driver.getDriver());
     @When("user click to Calendar module")
     public void userClickToCalendarModule() {
         navigateToModule("Calendar");
@@ -51,7 +51,7 @@ public class US005_StepDef extends BasePage {
 
     @Then("user should display daily calendar view")
     public void user_should_display_daily_calendar_view() {
-// verify that the daily calendar view is displayed
+
 
 // Get the current URL
         String currentUrl = Driver.getDriver().getCurrentUrl();
@@ -64,6 +64,119 @@ public class US005_StepDef extends BasePage {
 
         // Assert that the daily calendar view is displayed
         Assert.assertTrue("Daily calendar view is not displayed", isDailyCalendarViewDisplayed);
+    }
+
+
+
+    @When("user is on the Calendar page")
+    public void userIsOnTheCalendarPage() {
+        navigateToModule("Calendar");
+    }
+
+    @And("user clicks to the dropdown")
+    public void userClicksToTheDropdown() {
+        calendarPage.dropdown.click();
+
+    }
+
+    @And("user select to Week option")
+    public void userSelectToWeekOption() {
+        calendarPage.selectOptionByIndex(2);
+
+    }
+
+    @Then("user should display weekly calendar view")
+    public void userShouldDisplayWeeklyCalendarView() {
+        calendarPage.isDisplayedByText("Week");
+
+    }
+
+    @And("user select to Month option")
+    public void userSelectToMonthOption() {
+        calendarPage.selectOptionByIndex(3);
+    }
+
+    @Then("user should display monthly calendar view")
+    public void userShouldDisplayMonthlyCalendarView() {
+
+        String actualURL= Driver.getDriver().getCurrentUrl();
+        String expectedURL = "GridMonth/";
+
+        Assert.assertTrue(actualURL.contains(expectedURL));
+    }
+
+    @Given("the user is logged in as {string}")
+    public void theUserIsLoggedInAs(String userType) {
+        if (userType.equalsIgnoreCase("User")) {
+            new LoginPage().userLoginWithEnv();
+        } else if (userType.equalsIgnoreCase("Employee")) {
+            new LoginPage().employeeLoginWithEnv();
+        }
+
+    }
+
+    @When("the user is on the Calendar page")
+    public void theUserIsOnTheCalendarPage() {
+        navigateToModule("Calendar");
+    }
+
+    @And("the user creates a new event")
+    public void theUserCreatesANewEvent() {
+        calendarPage.newEventButton.click();
+
+    }
+
+    @And("the user enters the {string}")
+    public void theUserEntersThe(String eventName) {
+        this.eventName=eventName;
+        BrowserUtils.sleep(5);
+        calendarPage.enterEventTitle.sendKeys(eventName);
+    }
+
+
+    @And("the user selects the event start time")
+    public void theUserSelectsTheEventStartTime() {
+//        calendarPage.startDateAndTime.click();
+//        BrowserUtils.waitFor(5);
+//        calendarPage.startDate.click();
+
+        actions.moveToElement(calendarPage.startBox).perform();
+        actions.click(calendarPage.startBox).perform();
+
+
+
+
+    }
+
+    @And("And the user selects the event end time")
+    public void andTheUserSelectsTheEventEndTime() {
+//        calendarPage.endBox.click();
+//        BrowserUtils.waitFor(5);
+//        calendarPage.endDate.click();
+
+        actions.moveToElement(calendarPage.endBox).perform();
+        actions.click(calendarPage.endBox).perform();
+
+
+    }
+
+    @And("the user clicks the Save button")
+    public void theUserClicksTheSaveButton() {
+        calendarPage.saveButton.click();
+    }
+
+    @Then("user see the event on the related day through the Montly Calendar view")
+    public void userSeeTheEventOnTheRelatedDayThroughTheMontlyCalendarView() {
+        calendarPage.personalOption.click();
+
+        BrowserUtils.waitFor(3);
+
+      //  Assert.assertTrue(calendarPage.verifyEvent(eventName).isDisplayed());
+
+
+
+
+
     }
 
 
--- src/test/resources/features/US005_CalendarModule.feature
+++ src/test/resources/features/US005_CalendarModule.feature
@@ -1,7 +1,6 @@
 @B33G5-123
 Feature: Calendar Module Functionality
 
-
   @B33G5-119
   Scenario Outline:
     Given the user logged in as "<userType>"
@@ -14,3 +13,42 @@ Feature: Calendar Module Functionality
       | user     |
       | employee |
 
+
+  @B33G5-120
+  Scenario Outline: Verify user can display weekly calendar view
+    Given the user logged in as "<userType>"
+    When user is on the Calendar page
+    And user clicks to the dropdown
+    And user select to Week option
+    Then user should display weekly calendar view
+    Examples:
+      | userType |
+      | user     |
+      | employee |
+
+  @B33G5-121
+  Scenario Outline: Verify user can display monthly calendar view
+    Given the user logged in as "<userType>"
+    When user is on the Calendar page
+    When user click to the dropdown
+    And user select to Month option
+    Then user should display monthly calendar view
+    Examples:
+      | userType |
+      | user     |
+      | employee |
+
+  @B33G5-122
+  Scenario Outline:Verify user can create a new event under the Calendar module and see it on the related day through the Monthly Calendar view
+    Given the user logged in as "<userType>"
+    When the user is on the Calendar page
+    And the user creates a new event
+    And the user enters the "sprint meeting"
+    And the user selects the event start time
+    And And the user selects the event end time
+    And the user clicks the Save button
+    Then user see the event on the related day through the Montly Calendar view
+    Examples:
+      | userType |
+      | user     |
+      | employee |
