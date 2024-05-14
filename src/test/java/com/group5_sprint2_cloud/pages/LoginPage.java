9fd62dac294332e144303c670adb1739261f373b
cbanavi <cbanavi@gmail.com>
2024-05-14 10:38:43 -0700

    Cleaned up Login Page

--- src/test/java/com/group5_sprint2_cloud/pages/LoginPage.java
+++ src/test/java/com/group5_sprint2_cloud/pages/LoginPage.java
@@ -5,19 +5,20 @@ import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
 
-    public LoginPage(){
+    public LoginPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(id="user")
+    @FindBy(id = "user")
     public WebElement userName;
 
-    @FindBy(id="password")
+    @FindBy(id = "password")
     public WebElement passwordInput;
 
-    @FindBy(id ="submit-form")
+    @FindBy(id = "submit-form")
     public WebElement submit;
 
     public void login(String username, String password) {
@@ -35,7 +36,7 @@ public class LoginPage {
     /**
      * If you're logging in with User username, it will come to this method and pull the "USER_USERNAME" from your system
      */
-    public void userLoginWithEnv(){
+    public void userLoginWithEnv() {
         String usernameEnv = System.getenv("USER_USERNAME");
         String passwordEnv = System.getenv("USER_PASSWORD");
 
@@ -46,11 +47,12 @@ public class LoginPage {
     /**
      * If you're logging in with Employee username, it will come to this method and pull the "EMPLOYEE_USERNAME" from your system
      */
-    public void employeeLoginWithEnv(){
+    public void employeeLoginWithEnv() {
         String usernameEnv = System.getenv("EMPLOYEE_USERNAME");
         String passwordEnv = System.getenv("EMPLOYEE_PASSWORD");
 
         loginNoProperties(usernameEnv, passwordEnv);
 
     }
+
 }
