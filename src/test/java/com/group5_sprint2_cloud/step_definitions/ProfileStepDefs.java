938606cb875229f4d21d5e3a42f64de37445b083
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-05-15 23:25:21 -0700

    stepsdfefs

---
+++ src/test/java/com/group5_sprint2_cloud/step_definitions/ProfileStepDefs.java
@@ -0,0 +1,60 @@
+package com.group5_sprint2_cloud.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProfileStepDefs {
+
+    @When("user click to the Avatar")
+    public void user_click_to_the_avatar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user click to the Settings")
+    public void user_click_to_the_settings() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see the same name and username")
+    public void user_should_see_the_same_name_and_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user is on the Settings page")
+    public void the_user_is_on_the_settings_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user enters a proper email address to email input box")
+    public void the_user_enters_a_proper_email_address_to_email_input_box() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the email field should display user's proper address")
+    public void the_email_field_should_display_user_s_proper_address() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user's preferred language is {string}")
+    public void the_user_s_preferred_language_is(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user selects a language {string}")
+    public void the_user_selects_a_language(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the language field should display {string}")
+    public void the_language_field_should_display(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("website's language changed to {string}")
+    public void website_s_language_changed_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
