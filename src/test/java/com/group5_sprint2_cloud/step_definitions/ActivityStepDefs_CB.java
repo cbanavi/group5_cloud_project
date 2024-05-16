306e648880cb1704b3a36276b177ab2a867b4b44
Chiyavan Banavi <cbanavi@gmail.com>
2024-05-15 20:26:14 -0700

    Made my end of page message code dynamic so it scrolls down until message is reached

--- src/test/java/com/group5_sprint2_cloud/step_definitions/ActivityStepDefs_CB.java
+++ src/test/java/com/group5_sprint2_cloud/step_definitions/ActivityStepDefs_CB.java
@@ -3,10 +3,14 @@ package com.group5_sprint2_cloud.step_definitions;
 import com.group5_sprint2_cloud.pages.BasePage;
 import com.group5_sprint2_cloud.pages.SymundActivityPage_CB;
 import com.group5_sprint2_cloud.utilities.BrowserUtils;
+import com.group5_sprint2_cloud.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,6 +20,8 @@ import java.util.List;
 public class ActivityStepDefs_CB extends BasePage {
 
     SymundActivityPage_CB symundActivityPageCb = new SymundActivityPage_CB();
+
+    JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
     @When("User clicks Activity module")
     public void user_clicks_activity_module() {
@@ -41,7 +47,6 @@ public class ActivityStepDefs_CB extends BasePage {
 
     @Then("User should see all items list ordered by newest to oldest")
     public void userShouldSeeAllItemsListOrderedByNewestToOldest() {
-        BrowserUtils.waitForVisibility(symundActivityPageCb.messageAtBottomOfPage, 5);
 
         List<String> actualActivityTimes = new ArrayList<>();
 
@@ -63,7 +68,10 @@ public class ActivityStepDefs_CB extends BasePage {
 
     @Then("User should see {string} at the bottom of page")
     public void userShouldSeeAtTheBottomOfPage(String expectedMessage) {
-        BrowserUtils.waitForVisibility(symundActivityPageCb.messageAtBottomOfPage, 5);
+
+        while (!symundActivityPageCb.messageAtBottomOfPage.isDisplayed()){
+            new Actions(Driver.getDriver()).sendKeys(Keys.PAGE_DOWN).perform();
+        }
 
         Assert.assertEquals(expectedMessage, symundActivityPageCb.messageAtBottomOfPage.getText());
 
