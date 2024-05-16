eac4395d0c659bc9d30390104cd3d7418e7170d5
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-05-16 12:15:28 -0700

    updated

--- src/test/java/com/group5_sprint2_cloud/step_definitions/ProfileStepDefs.java
+++ src/test/java/com/group5_sprint2_cloud/step_definitions/ProfileStepDefs.java
@@ -1,6 +1,7 @@
 package com.group5_sprint2_cloud.step_definitions;
 
 import com.group5_sprint2_cloud.pages.ProfileSettingsPage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -8,8 +9,9 @@ import org.junit.Assert;
 public class ProfileStepDefs {
 
     ProfileSettingsPage profileSettingsPage = new ProfileSettingsPage();
+    String userProperEmail = "admin@example.com";
 
-    @When("user click to the Avatar")
+    @When("user click to the menu button")
     public void user_click_to_the_avatar() {
         // Write code here that turns the phrase above into concrete actions
         profileSettingsPage.buttonExpandMenu.click();
@@ -37,37 +39,26 @@ public class ProfileStepDefs {
     @When("the user enters a proper email address to email input box")
     public void the_user_enters_a_proper_email_address_to_email_input_box() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        profileSettingsPage.inputEmailField.clear();
+        profileSettingsPage.inputEmailField.sendKeys(userProperEmail);
     }
 
     @Then("the email field should display user's proper address")
     public void the_email_field_should_display_user_s_proper_address() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertEquals(profileSettingsPage.inputEmailField.getAttribute("value"), userProperEmail);
     }
 
-    @When("the user's preferred language is {string}")
-    public void the_user_s_preferred_language_is(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the user selects a language {string}")
+    @And("the user selects a language {string}")
     public void the_user_selects_a_language(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        profileSettingsPage.selectLanguageByValue(string);
+        Assert.assertEquals(profileSettingsPage.getSelectedLanguageValue(), string);
     }
 
     @Then("the language field should display {string}")
     public void the_language_field_should_display(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertEquals(profileSettingsPage.getSelectedLanguageText(), string);
     }
-
-    @Then("website's language changed to {string}")
-    public void website_s_language_changed_to(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
 }
--- src/test/resources/features/US014_ProfileSettings.feature
+++ src/test/resources/features/US014_ProfileSettings.feature
@@ -5,14 +5,13 @@ Feature: Check/Change Profile Settings Functionality
   Scenario Outline: Full name and username should be the same
     Given the user logged in as "<userType>"
     When the user is on the Settings page
-    When user click to the Avatar
+    When user click to the menu button
     And  user click to the Settings
     Then user should see the same name and username
     Examples:
       | userType |
       | user     |
       | employee |
-
 
   @B33G5-177
   Scenario Outline: User can enter a proper e-mail to Email input box.
@@ -29,10 +28,10 @@ Feature: Check/Change Profile Settings Functionality
   Scenario Outline: Verify user can change Language.
     Given the user logged in as "<userType>"
     When the user is on the Settings page
-    And the user's preferred language is "English (US)"
-    When the user selects a language "Español (España)"
+    And the user selects a language "en"
+    Then the language field should display "English (US)"
+    When the user selects a language "es"
     Then the language field should display "Español (España)"
-    And website's language changed to "Español (España)"
     Examples:
       | userType |
       | user     |
