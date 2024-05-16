760853dd4aed4c5afd8f7b5f3113903a2ab4f44a
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-05-16 01:29:00 -0700

    change xpath, optimize imports , reformat code

--- src/test/java/com/group5_sprint2_cloud/pages/ProfileSettingsPage.java
+++ src/test/java/com/group5_sprint2_cloud/pages/ProfileSettingsPage.java
@@ -1,15 +1,11 @@
 package com.group5_sprint2_cloud.pages;
+
 import com.group5_sprint2_cloud.utilities.BrowserUtils;
 import com.group5_sprint2_cloud.utilities.Driver;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
-
-import java.util.List;
 
 public class ProfileSettingsPage {
 
@@ -17,22 +13,22 @@ public class ProfileSettingsPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(id="displayname")
+    @FindBy(id = "displayname")
     public WebElement userFullName;
 
-    @FindBy(xpath = "//div[@id='settings']//span[@title]/@title")
+    @FindBy(xpath = "//div[@id='settings']//span[@title]")
     public WebElement userName;
 
-    @FindBy(id="expand")
+    @FindBy(id = "expand")
     public WebElement buttonExpandMenu;
 
     @FindBy(xpath = "//li[@data-id=\"settings\"]//a")
     public WebElement linkSettings;
 
-    @FindBy(id="email")
+    @FindBy(id = "email")
     public WebElement inputEmailField;
 
-    @FindBy(id="languageinput")
+    @FindBy(id = "languageinput")
     public WebElement dropdownLanguage;
 
     public void goToProfileSettingPage() {
@@ -46,7 +42,7 @@ public class ProfileSettingsPage {
             // TODO trow error that linkSettings not displayed
         }
         String expectedUrl = "https://qa.symund.com/index.php/settings/user";
-        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),expectedUrl);
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), expectedUrl);
     }
 
 }
