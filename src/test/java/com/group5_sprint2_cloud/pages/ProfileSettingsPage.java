798ed302c8f408db0e30f6128066f8a90be90398
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-05-16 00:44:30 -0700

    add webelements and goToProfileSettingPage

---
+++ src/test/java/com/group5_sprint2_cloud/pages/ProfileSettingsPage.java
@@ -0,0 +1,52 @@
+package com.group5_sprint2_cloud.pages;
+
+import com.group5_sprint2_cloud.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class ProfileSettingsPage {
+
+    public ProfileSettingsPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id="displayname")
+    public WebElement userFullName;
+
+    @FindBy(xpath = "//div[@id='settings']//span[@title]/@title")
+    public WebElement userName;
+
+    @FindBy(id="expand")
+    public WebElement buttonExpandMenu;
+
+    @FindBy(xpath = "//a[text()=\"Settings\"]")
+    public WebElement linkSettings;
+
+    @FindBy(id="email")
+    public WebElement inputEmailField;
+
+    @FindBy(id="languageinput")
+    public WebElement dropdownLanguage;
+
+    public void goToProfileSettingPage() {
+        buttonExpandMenu.click();
+        if (linkSettings.isDisplayed()) {
+            linkSettings.click();
+        } else {
+            // TODO trow error that linkSettings not displayed
+        }
+        String expectedUrl = "https://qa.symund.com/index.php/settings/user";
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),expectedUrl);
+    }
+
+}
+
+
+
