9b194c9185c592816c6eb85ba0f28f25b1002f2a
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-05-16 00:57:30 -0700

    @When("the user is on the Settings page")

--- src/test/java/com/group5_sprint2_cloud/step_definitions/ProfileStepDefs.java
+++ src/test/java/com/group5_sprint2_cloud/step_definitions/ProfileStepDefs.java
@@ -2,6 +2,7 @@ package com.group5_sprint2_cloud.step_definitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import com.group5_sprint2_cloud.pages.ProfileSettingsPage;
 
 public class ProfileStepDefs {
 
@@ -23,7 +24,8 @@ public class ProfileStepDefs {
     @When("the user is on the Settings page")
     public void the_user_is_on_the_settings_page() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        new ProfileSettingsPage().goToProfileSettingPage();
+
     }
     @When("the user enters a proper email address to email input box")
     public void the_user_enters_a_proper_email_address_to_email_input_box() {
