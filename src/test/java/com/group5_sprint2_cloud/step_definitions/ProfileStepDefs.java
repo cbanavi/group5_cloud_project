9288525de08dd57da62df0b751da7aa8c164915c
Konstantin Gavrilov <gavrilovks@gmail.com>
2024-05-16 01:29:41 -0700

    updated

--- src/test/java/com/group5_sprint2_cloud/step_definitions/ProfileStepDefs.java
+++ src/test/java/com/group5_sprint2_cloud/step_definitions/ProfileStepDefs.java
@@ -1,37 +1,45 @@
 package com.group5_sprint2_cloud.step_definitions;
 
+import com.group5_sprint2_cloud.pages.ProfileSettingsPage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import com.group5_sprint2_cloud.pages.ProfileSettingsPage;
+import org.junit.Assert;
 
 public class ProfileStepDefs {
+
+    ProfileSettingsPage profileSettingsPage = new ProfileSettingsPage();
 
     @When("user click to the Avatar")
     public void user_click_to_the_avatar() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        profileSettingsPage.buttonExpandMenu.click();
     }
+
     @When("user click to the Settings")
     public void user_click_to_the_settings() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        profileSettingsPage.linkSettings.click();
     }
+
     @Then("user should see the same name and username")
     public void user_should_see_the_same_name_and_username() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertEquals(profileSettingsPage.userFullName.getAttribute("value"), profileSettingsPage.userName.getAttribute("title"));
     }
+
     @When("the user is on the Settings page")
     public void the_user_is_on_the_settings_page() {
         // Write code here that turns the phrase above into concrete actions
-        new ProfileSettingsPage().goToProfileSettingPage();
+        profileSettingsPage.goToProfileSettingPage();
 
     }
+
     @When("the user enters a proper email address to email input box")
     public void the_user_enters_a_proper_email_address_to_email_input_box() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Then("the email field should display user's proper address")
     public void the_email_field_should_display_user_s_proper_address() {
         // Write code here that turns the phrase above into concrete actions
@@ -43,16 +51,19 @@ public class ProfileStepDefs {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @When("the user selects a language {string}")
     public void the_user_selects_a_language(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Then("the language field should display {string}")
     public void the_language_field_should_display(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Then("website's language changed to {string}")
     public void website_s_language_changed_to(String string) {
         // Write code here that turns the phrase above into concrete actions
