96d17f2010588e7eef854756e900b9907bbd2afe
Mohamed Sami Melakhessou <melakhessou.sami@gmail.com>
2024-05-18 01:04:02 -0700

    update

--- src/test/java/com/group5_sprint2_cloud/step_definitions/Dashboard_Functionality_StepDefs.java
+++ src/test/java/com/group5_sprint2_cloud/step_definitions/Dashboard_Functionality_StepDefs.java
@@ -1,92 +1,81 @@
 package com.group5_sprint2_cloud.step_definitions;
 
-
 import com.group5_sprint2_cloud.pages.CustomizePage;
 import com.group5_sprint2_cloud.pages.DashboardPage;
-import com.group5_sprint2_cloud.pages.LoginPage;
 import com.group5_sprint2_cloud.pages.SetStatusPage;
 import com.group5_sprint2_cloud.utilities.BrowserUtils;
-import com.group5_sprint2_cloud.utilities.ConfigurationReader;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
+import org.junit.Assert;
+
+
 
 public class Dashboard_Functionality_StepDefs {
-
-
 
     DashboardPage dashboardPage = new DashboardPage();
     SetStatusPage setStatusPage = new SetStatusPage();
     CustomizePage customizePage = new CustomizePage();
 
-    /*
-
-    String username = ConfigurationReader.getProperty("username");
-    String password = ConfigurationReader.getProperty("password");
-    String employer =ConfigurationReader.getProperty("employee");
-    String employee_password = ConfigurationReader.getProperty("employee_password");
-
-    @Given("the user is logged in")
-    public void the_user_logged_in_as() {
-
-        loginPage.login(employer, employee_password);
-
-
-    }
-
-     */
-
     @When("the user accesses the dashboard")
     public void the_user_accesses_the_dashboard() {
-
         BrowserUtils.verifyTitle("Dashboard - Symund - QA");
-
     }
 
     @Then("the user should see all modules")
     public void the_user_should_see_all_modules() {
-
-        BrowserUtils.verifyElementDisplayed(dashboardPage.dashboard);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.files);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.photos);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.activity);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.talk);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.mail);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.contact);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.circle);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.calendar);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.notes);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.deck);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.tasks);
-
-
+        verifyDashboardModules();
     }
 
     @Then("the user's username should be displayed")
     public void the_user_s_username_should_be_displayed() {
-
-
         dashboardPage.userButton.click();
-        BrowserUtils.sleep(3);
-        BrowserUtils.verifyElementDisplayed(dashboardPage.userName);
-
+        BrowserUtils.sleep(1);
+        Assert.assertTrue("User name is not displayed", dashboardPage.userName.isDisplayed());
     }
-
 
     @When("the user clicks on the Customize button")
     public void the_user_clicks_on_the_customize_button() {
-
         dashboardPage.customizeButton.click();
-        BrowserUtils.sleep(3);
-
+        BrowserUtils.sleep(1);
     }
 
     @When("selects a widget")
     public void selects_a_widget() {
+        selectWidgets();
+    }
 
+    @Then("the selected widget should be displayed")
+    public void the_selected_widget_should_be_displayed() {
+        verifyWidgetsDisplayed();
+        customizePage.closeButton.click();
+    }
 
+    @When("the user clicks on the Set Status button")
+    public void the_user_clicks_on_the_set_status_button() {
+        dashboardPage.setStatusButton.click();
+    }
 
+    @When("selects a status option")
+    public void selects_a_status_option() {
+        selectStatusOptions();
+    }
+
+    private void verifyDashboardModules() {
+        Assert.assertTrue("Dashboard is displayed", dashboardPage.dashboard.isDisplayed());
+        Assert.assertTrue("Files is displayed", dashboardPage.files.isDisplayed());
+        Assert.assertTrue("Photos is displayed", dashboardPage.photos.isDisplayed());
+        Assert.assertTrue("Activity is displayed", dashboardPage.activity.isDisplayed());
+        Assert.assertTrue("Talk is displayed", dashboardPage.talk.isDisplayed());
+        Assert.assertTrue("Mail is displayed", dashboardPage.mail.isDisplayed());
+        Assert.assertTrue("Contact is displayed", dashboardPage.contact.isDisplayed());
+        Assert.assertTrue("Circle is displayed", dashboardPage.circle.isDisplayed());
+        Assert.assertTrue("Calendar is displayed", dashboardPage.calendar.isDisplayed());
+        Assert.assertTrue("Notes is displayed", dashboardPage.notes.isDisplayed());
+        Assert.assertTrue("Deck is displayed", dashboardPage.deck.isDisplayed());
+        Assert.assertTrue("Tasks is displayed", dashboardPage.tasks.isDisplayed());
+    }
+
+    private void selectWidgets() {
         customizePage.weather.click();
         customizePage.upcomingCards.click();
         customizePage.recommendedFiles.click();
@@ -96,49 +85,27 @@ public class Dashboard_Functionality_StepDefs {
         customizePage.importantMail.click();
         customizePage.UnreadMail.click();
         customizePage.notes.click();
-
-
-
-
     }
 
-    @Then("the selected widget should be displayed")
-    public void the_selected_widget_should_be_displayed() {
-
-        BrowserUtils.verifyElementDisplayed(customizePage.status);
-        BrowserUtils.verifyElementDisplayed(customizePage.weather);
-        BrowserUtils.verifyElementDisplayed(customizePage.upcomingCards);
-        BrowserUtils.verifyElementDisplayed(customizePage.recommendedFiles);
-        BrowserUtils.verifyElementDisplayed(customizePage.talkMentions);
-        BrowserUtils.verifyElementDisplayed(customizePage.upcomingEvents);
-        BrowserUtils.verifyElementDisplayed(customizePage.recentStatues);
-        BrowserUtils.verifyElementDisplayed(customizePage.importantMail);
-        BrowserUtils.verifyElementDisplayed(customizePage.UnreadMail);
-        BrowserUtils.verifyElementDisplayed(customizePage.notes);
-
-        customizePage.closeButton.click();
-
-
+    private void verifyWidgetsDisplayed() {
+        Assert.assertTrue("Status widget is displayed", customizePage.status.isDisplayed());
+        Assert.assertTrue("Weather widget is displayed", customizePage.weather.isDisplayed());
+        Assert.assertTrue("Upcoming Cards widget is  displayed", customizePage.upcomingCards.isDisplayed());
+        Assert.assertTrue("Recommended Files widget is displayed", customizePage.recommendedFiles.isDisplayed());
+        Assert.assertTrue("Talk Mentions widget is  displayed", customizePage.talkMentions.isDisplayed());
+        Assert.assertTrue("Upcoming Events widget is displayed", customizePage.upcomingEvents.isDisplayed());
+        Assert.assertTrue("Recent Statues widget is displayed", customizePage.recentStatues.isDisplayed());
+        Assert.assertTrue("Important Mail widget is displayed", customizePage.importantMail.isDisplayed());
+        Assert.assertTrue("Unread Mail widget is displayed", customizePage.UnreadMail.isDisplayed());
+        Assert.assertTrue("Notes widget is displayed", customizePage.notes.isDisplayed());
     }
 
-
-    @When("the user clicks on the Set Status button")
-    public void the_user_clicks_on_the_set_status_button() {
-
-
-        dashboardPage.setStatusButton.click();
-
-    }
-
-    @When("selects a status option")
-    public void selects_a_status_option() {
-
+    private void selectStatusOptions() {
         setStatusPage.away.click();
         setStatusPage.doNotDisturb.click();
         setStatusPage.invisible.click();
         BrowserUtils.sleep(1);
         setStatusPage.online.click();
         setStatusPage.closeButton.click();
-
     }
 }
