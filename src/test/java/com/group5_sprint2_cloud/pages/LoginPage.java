0934c4a5dc03616dfacc6064b54d9dd2b7bd18f1
cbanavi <109002559+cbanavi@users.noreply.github.com>
2024-05-18 11:30:15 -0700

    Update LoginPage.java

--- src/test/java/com/group5_sprint2_cloud/pages/LoginPage.java
+++ src/test/java/com/group5_sprint2_cloud/pages/LoginPage.java
@@ -37,34 +37,6 @@ public class LoginPage {
      submit.click();
      }
 
-
-     // If you're logging in with User username, it will come to this method and pull the "USER_USERNAME" from your system
-
-     public void userLoginWithEnv() {
-     String usernameEnv = System.getenv("USER_USERNAME");
-     String passwordEnv = System.getenv("USER_PASSWORD");
-
-     loginNoProperties(usernameEnv, passwordEnv);
-
-     }
-
-
-     //If you're logging in with Employee username, it will come to this method and pull the "EMPLOYEE_USERNAME" from your system
-
-     public void employeeLoginWithEnv() {
-     String usernameEnv = System.getenv("EMPLOYEE_USERNAME");
-     String passwordEnv = System.getenv("EMPLOYEE_PASSWORD");
-
-     loginNoProperties(usernameEnv, passwordEnv);
-
-     }
-
-     public void loginNoProperties(String username, String password) {
-     userName.sendKeys(username);
-     passwordInput.sendKeys(password);
-     submit.click();
-     }
-
      //If you're logging in with User username, it will come to this method and pull the "USER_USERNAME" from your system
 
      public void userLoginWithEnv() {
